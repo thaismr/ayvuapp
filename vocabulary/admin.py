1628c30e5fd1fcf8364b27f71773410afd8fbdec
Thaís Mendes Ramalho <thaismr@users.noreply.github.com>
2022-05-05 14:04:05 -0300

    Layout experiments and admin autocomplete fields.

--- vocabulary/admin.py
+++ vocabulary/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Vocabulary
 
-admin.site.register(Vocabulary)
+
+@admin.register(Vocabulary)
+class VocabularyAdmin(admin.ModelAdmin):
+    search_fields = ['word', 'definition', 'language']
+    autocomplete_fields = ['synonyms']
