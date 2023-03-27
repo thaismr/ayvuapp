4bcbfad52dcabce822bef9a4dbedd730e8f74141
Thaís Mendes Ramalho <thaismr@users.noreply.github.com>
2023-03-27 20:45:11 -0300

    Vocabulary: new models.

--- vocabulary/admin.py
+++ vocabulary/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
-from .models import Vocabulary
+from .models import (
+    Vocabulary,
+    PartOfSpeech,
+    VocabularyDefinition,
+    VocabularyExample
+)
 
 
 @admin.register(Vocabulary)
 class VocabularyAdmin(admin.ModelAdmin):
-    search_fields = ['word', 'definition']
+    search_fields = ['word', ]
     autocomplete_fields = ('synonyms',)
+
+
+@admin.register(PartOfSpeech)
+class PartOfSpeechAdmin(admin.ModelAdmin):
+    search_fields = ['part_of_speech', ]
+
+
+@admin.register(VocabularyDefinition)
+class VocabularyDefinitionAdmin(admin.ModelAdmin):
+    search_fields = ['definition', 'usage']
+
+
+@admin.register(VocabularyExample)
+class VocabularyExampleAdmin(admin.ModelAdmin):
+    search_fields = ['example', ]
+
