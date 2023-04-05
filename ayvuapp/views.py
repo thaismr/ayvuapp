6ceabab35fb096e0bd758db2127d300b68bdccef
Thaís Mendes Ramalho <thaismr@users.noreply.github.com>
2023-04-05 12:45:02 -0300

    Home: resource list as start page for auth users

--- ayvuapp/views.py
+++ ayvuapp/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import redirect, render
 
 def home(request):
     if request.user.is_authenticated:
-        return redirect('resource:create')
+        return redirect('resource:list')
     else:
         return redirect('login')
